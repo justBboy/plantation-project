8b9932079e92f3d1286650770fc426af5bf65fd7
Bboy <bornscience1@gmail.com>
2020-11-24 15:43:14 -0800

    styled certificate and added favicon

--- static/js/donate-cert.js
+++ static/js/donate-cert.js
@@ -9,28 +9,46 @@ let username = canvas.dataset.username,
     city = canvas.dataset.city,
     amount = canvas.dataset.amount;
 
+let logo = new Image();
+logo.src = globalFiles.logo;
+logo.width = 50
+logo.height = 50;
+
+console.dir(logo)
+
+logo.onload = () => {
+    ctx.drawImage(logo, WIDTH/2 - 25, 80)
+}
+
+
 console.log(username, city, amount)
 
 ctx.beginPath()
-ctx.fillStyle = "#2e8";
+ctx.fillStyle = "#ddd";
+ctx.rect(0, 0, WIDTH, HEIGHT);
+ctx.fill();
+ctx.closePath()
+
+ctx.beginPath()
+ctx.fillStyle = "#000050";
 ctx.rect(0, 0, WIDTH, 40)
 ctx.fill()
 ctx.closePath();
 
 ctx.beginPath();
-ctx.fillStyle = "#2e8";
+ctx.fillStyle = "#000050";
 ctx.rect(0, 40, 40, HEIGHT);
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.fillStyle = "#2e8";
+ctx.fillStyle = "#000050";
 ctx.rect(40, HEIGHT - 40, WIDTH, 40);
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.fillStyle = "#2e8";
+ctx.fillStyle = "#000050";
 ctx.rect(WIDTH - 40, 0, 40, HEIGHT);
 ctx.fill();
 ctx.closePath();
@@ -59,34 +77,34 @@ ctx.rect(WIDTH-50, 50, 5, HEIGHT-100);
 ctx.fill();
 ctx.closePath();
 
-ctx.font = "60px Comic Sans Ms";
-ctx.fillStyle = "#2e8";
+ctx.font = "64px Impact";
+ctx.fillStyle = "#000050";
 ctx.textAlign = "center";
-ctx.fillText("Certificate", WIDTH/2, 140)
+ctx.fillText("Certificate", WIDTH/2, 180)
 
 ctx.font = "30px Sans Serif";
 ctx.textAlign = "center";
-ctx.fillText("of Donation", WIDTH/2, 180)
+ctx.fillText("of Donation", WIDTH/2, 220)
 
 ctx.font = "20px Comic Sans";
-ctx.fillStyle = "#4e7";
+ctx.fillStyle = "#882";
 ctx.textAlign = "center";
-ctx.fillText("This certifies that", WIDTH/2, 220)
+ctx.fillText("This certifies that", WIDTH/2, 250)
 
 ctx.font = "80px Montserrat";
-ctx.fillStyle = "#222";
+ctx.fillStyle = "#000050";
 ctx.textAlign = "center";
-ctx.fillText(username, WIDTH/2, 290)
+ctx.fillText(username.toUpperCase(), WIDTH/2, 320)
 
 ctx.font = "20px Comic Sans";
-ctx.fillStyle = "#4e7";
+ctx.fillStyle = "#882";
 ctx.textAlign = "center";
-ctx.fillText(`has successfully funded ${amount} rupees for`, WIDTH/2, 330);
+ctx.fillText(`has successfully funded ${amount} rupees for`, WIDTH/2, 350);
 
 ctx.font = "20px Comic Sans";
-ctx.fillStyle = "#4e7";
+ctx.fillStyle = "#882";
 ctx.textAlign = "center";
-ctx.fillText(`plantation in ${city}`, WIDTH/2, 350);
+ctx.fillText(`plantation in ${city}`, WIDTH/2, 380);
 
 ctx.font = "80px Comic Sans";
 ctx.fillStyle = "#111";
--- static/js/planted-cert.js
+++ static/js/planted-cert.js
@@ -8,28 +8,46 @@ let WIDTH = canvas.width,
 let username = canvas.dataset.username,
     city = canvas.dataset.city;
 
+let logo = new Image();
+logo.src = globalFiles.logo;
+logo.width = 50
+logo.height = 50;
+
+console.dir(logo)
+
+logo.onload = () => {
+    ctx.drawImage(logo, WIDTH/2 - 25, 80)
+}
+
+
 console.log(username, city)
 
 ctx.beginPath()
-ctx.fillStyle = "#2e8";
+ctx.fillStyle = "#ddd";
+ctx.rect(0, 0, WIDTH, HEIGHT);
+ctx.fill();
+ctx.closePath()
+
+ctx.beginPath()
+ctx.fillStyle = "#000050";
 ctx.rect(0, 0, WIDTH, 40)
 ctx.fill()
 ctx.closePath();
 
 ctx.beginPath();
-ctx.fillStyle = "#2e8";
+ctx.fillStyle = "#000050";
 ctx.rect(0, 40, 40, HEIGHT);
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.fillStyle = "#2e8";
+ctx.fillStyle = "#000050";
 ctx.rect(40, HEIGHT - 40, WIDTH, 40);
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.fillStyle = "#2e8";
+ctx.fillStyle = "#000050";
 ctx.rect(WIDTH - 40, 0, 40, HEIGHT);
 ctx.fill();
 ctx.closePath();
@@ -58,34 +76,34 @@ ctx.rect(WIDTH-50, 50, 5, HEIGHT-100);
 ctx.fill();
 ctx.closePath();
 
-ctx.font = "60px Comic Sans Ms";
-ctx.fillStyle = "#2e8";
+ctx.font = "64px Impact";
+ctx.fillStyle = "#000050";
 ctx.textAlign = "center";
-ctx.fillText("Certificate", WIDTH/2, 140)
+ctx.fillText("Certificate", WIDTH/2, 180)
 
 ctx.font = "30px Sans Serif";
 ctx.textAlign = "center";
-ctx.fillText("of Plantation", WIDTH/2, 180)
+ctx.fillText("of Donation", WIDTH/2, 220)
 
 ctx.font = "20px Comic Sans";
-ctx.fillStyle = "#4e7";
+ctx.fillStyle = "#882";
 ctx.textAlign = "center";
-ctx.fillText("This certifies that", WIDTH/2, 220)
+ctx.fillText("This certifies that", WIDTH/2, 250)
 
 ctx.font = "80px Montserrat";
-ctx.fillStyle = "#222";
+ctx.fillStyle = "#000050";
 ctx.textAlign = "center";
-ctx.fillText(username, WIDTH/2, 290)
+ctx.fillText(username.toUpperCase(), WIDTH/2, 320)
 
 ctx.font = "20px Comic Sans";
-ctx.fillStyle = "#4e7";
+ctx.fillStyle = "#882";
 ctx.textAlign = "center";
-ctx.fillText(`has successfully accepted to plant`, WIDTH/2, 330);
+ctx.fillText(`has successfully accepted to plant`, WIDTH/2, 350);
 
 ctx.font = "20px Comic Sans";
-ctx.fillStyle = "#4e7";
+ctx.fillStyle = "#882";
 ctx.textAlign = "center";
-ctx.fillText(`at ${city}`, WIDTH/2, 350);
+ctx.fillText(`at ${city}`, WIDTH/2, 380);
 
 ctx.font = "80px Comic Sans";
 ctx.fillStyle = "#111";
